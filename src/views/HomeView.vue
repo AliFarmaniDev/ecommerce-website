aed86c9f075d79a43f62ce9881a9da3047ce2bfa
Alireza <alirezafarmani619@gmail.com>
2024-12-04 09:48:25 -0800

    install tailwindCss and start project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h2>home page</h2>
+  </div>
 </template>
