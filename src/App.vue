9db7493eac305a365464d03734beaa64e38334a4
Alireza <alirezafarmani619@gmail.com>
2024-12-06 23:06:23 -0800

    create a responsive header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
-
+import { RouterView } from 'vue-router'
+//import header adn footer components
+import Header from './components/Header.vue'
 </script>
 
 <template>
-  <RouterView/>
+  <Header />
+  <RouterView />
 </template>
--- src/layout/Container.vue
+++ src/layout/Container.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="">
-        <slot></slot>
-    </div>
+  <!--this is main layout-->
+  <div class="mx-4 sm:mx-5 md:mx-6 my-4 sm:my-5 md:my-6">
+    <slot></slot>
+  </div>
 </template>
